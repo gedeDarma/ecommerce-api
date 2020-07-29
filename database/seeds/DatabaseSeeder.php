6cc440cd90b43cf75a5097c8abf5e1640df73899
unknown <de.darma.damuh@gmail.com>
2020-07-30 04:29:16 +0800

    Seeding Product and Review

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Model\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,5 +13,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UserSeeder::class);
+		factory(App\Model\Product::class,50)->create();
+		factory(App\Model\Review::class,300)->create();
     }
 }
